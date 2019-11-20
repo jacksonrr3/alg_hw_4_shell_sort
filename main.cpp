a6751cb0f711017784c1d46590fae691a6c968d8
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2019-11-20 13:27:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,36 +23,74 @@ void shell_sort(std::vector<T>& v, size_t n) {
 }
 
 template <typename T>
-void made_vec(std::vector<T>& v) {
+void made_vec_unsort(std::vector<T>& v) {
 	std::for_each(v.begin(), v.end(), [](T& a) {a = rand(); });
 }
 
+template <typename T>
+void made_vec_10persent(std::vector<T>& v) {
+	int n = v.size() / 10;
+	for (int i = 0; i < n; i++) {
+		v[i] = rand();
+	}
+}
+
+template <typename T>
+void made_vec_5persent(std::vector<T>& v) {
+	int n = v.size() * 5 / 100;
+	for (int i = 0; i < n; i++) {
+		v[i] = rand();
+	}
+}
+
+
 template<typename T>
-void test_sort_time(size_t d, const std::string& type) {
-	std::vector<T> v(1000000);
-	//заполняем вектор данными, в соответствии с функцией f
-	made_vec(v); 
+void test_sort_time(size_t d) {
+	std::vector<T> v(5000000);
 	std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+
+	//заполняем вектор слычайными данными
+	made_vec_unsort(v);
 	start = std::chrono::high_resolution_clock::now();
 	//сортировка вектора
 	shell_sort(v, d);
 	end = std::chrono::high_resolution_clock::now();
-	std::cout << type << ", d = " << d << ", time:" << std::chrono::duration_cast<std::chrono::milliseconds>
+	std::cout << "Sort unsorted vec, d = " << d << ", time:" << std::chrono::duration_cast<std::chrono::milliseconds>
 		(end - start).count() << " ms;" << std::endl;
 
+	//10 процентов данных не отсортированы
+	made_vec_10persent(v);
+	start = std::chrono::high_resolution_clock::now();
+	//сортировка вектора
+	shell_sort(v, d);
+	end = std::chrono::high_resolution_clock::now();
+	std::cout << "Sort 10 persent unsorted vec, d = " << d << ", time:" << std::chrono::duration_cast<std::chrono::milliseconds>
+		(end - start).count() << " ms;" << std::endl;
 
+	//5 процентов данных не отсортированы
+	made_vec_5persent(v);
+	start = std::chrono::high_resolution_clock::now();
+	//сортировка вектора
+	shell_sort(v, d);
+	end = std::chrono::high_resolution_clock::now();
+	std::cout << "Sort 5 persent unsorted vec, d = " << d << ", time:" << std::chrono::duration_cast<std::chrono::milliseconds>
+		(end - start).count() << " ms;" << std::endl;
 }
 
 
 
 int main() {
 	std::srand(std::time(nullptr));
+
+	//тест d=2
+	test_sort_time<int>(2);
+
+	//тест d=3
+	test_sort_time<int>(3);
 	
-	/*
-	made_vec(v1);
-	shell_sort(v1, 2);
-	*/
-	test_sort_time<int>(2, "random");
+	//тест d=4
+	test_sort_time<int>(4);
+
 
 	return 0;
 }
