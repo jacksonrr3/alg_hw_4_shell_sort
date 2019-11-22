a60198ee7b65a899dc8dc01888639724360358bf
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2019-11-22 23:16:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,17 +36,17 @@ void made_vec_10persent(std::vector<T>& v) {
 }
 
 template <typename T>
-void made_vec_5persent(std::vector<T>& v) {
+void made_vec_5(std::vector<T>& v) {
 	int n = v.size() * 5 / 100;
-	for (int i = 0; i < n; i++) {
-		v[i] = rand();
+	for (int i = 0; i < 4; i++) {
+		v[rand() % v.size()] = rand();
 	}
 }
 
 
 template<typename T>
 void test_sort_time(size_t g) {
-	std::vector<T> v(5000000);
+	std::vector<T> v(1000000);
 	std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
 
 	//заполняем вектор слычайными данными
@@ -68,7 +68,7 @@ void test_sort_time(size_t g) {
 		(end - start).count() << " ms;" << std::endl;
 
 	//5 процентов данных не отсортированы
-	made_vec_5persent(v);
+	made_vec_5(v);
 	start = std::chrono::high_resolution_clock::now();
 	//сортировка вектора
 	shell_sort(v, g);
@@ -85,19 +85,27 @@ int main() {
 	//тест d=2
 	test_sort_time<int>(2);
 	// unsorted_vec   =  34264 mc
-	
-	
+
+
 	//тест d=3
 	test_sort_time<int>(3);
-	
+
 	//тест d=4
 	test_sort_time<int>(4);
-	
-	
+
+	//тест d=5
+	test_sort_time<int>(5);
+
+	//тест d=6
+	test_sort_time<int>(6);
+
+	//тест d=6
+	test_sort_time<int>(7);
+
 	//		d(gap) =      2	           3           4
 	// unsorted_vec   	=  34264ms 	30380ms	    33521ms
 	// unsort_10_persent	=  22380ms 	21594ms	    19301ms	
 	// unsort_5_persent	=  18862ms	19713ms     18686ms
-	
+
 	return 0;
 }
